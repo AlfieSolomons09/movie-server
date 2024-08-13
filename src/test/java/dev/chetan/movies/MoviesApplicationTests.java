ee9a17f2593dc1d86176515518df2b0e8b707816
Chetan Choudhary <98541541+AlfieSolomons09@users.noreply.github.com>
2024-08-13 16:52:02 +0530

    inital commit

---
+++ src/test/java/dev/chetan/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+//package dev.chetan.movies;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MoviesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
